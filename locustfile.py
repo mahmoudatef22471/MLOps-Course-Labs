41521ee5bfc0c7e87ae2b6b07364636080801ce2
mahmoudatef22471-debug <mahmoudatef22471@gmail.com>
2026-05-16 12:04:32 +0300

    editting the deployment job to get the docker image from ECR registry

---
+++ locustfile.py
@@ -0,0 +1,31 @@
+from locust import HttpUser, task, between
+
+
+class ChurnPredictionUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def test_home(self):
+        self.client.get("/")
+
+    @task(3)
+    def test_health(self):
+        self.client.get("/health")
+
+    @task
+    def test_predict(self):
+        self.client.post(
+            "/predict",
+            json={
+                "CreditScore": 600,
+                "Geography": "France",
+                "Gender": "Male",
+                "Age": 40,
+                "Tenure": 5,
+                "Balance": 50000.0,
+                "NumOfProducts": 2,
+                "HasCrCard": 1,
+                "IsActiveMember": 1,
+                "EstimatedSalary": 100000.0,
+            },
+        )
